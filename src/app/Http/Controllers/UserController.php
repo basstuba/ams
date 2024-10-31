af5957c7ad919323f8882dea3ea5b959613482a0
nutka <tuba4080@outlook.com>
2024-10-31 22:32:48 +0100

    UserController作成

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Exception;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request) {
+        try {
+            User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+            ]);
+
+            return response()->json(['message' => '登録完了しました'], 201);
+        } catch (Exception $error) {
+            return response()->json(['error' => 'サーバーエラーが発生しました'], 500);
+        }
+    }
+
+    public function login() {
+        $credentials = request(['email', 'password']);
+
+        if( !$token = auth()->attempt($credentials) ) {
+            return response()->json(['error' => '認証に失敗しました'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        return response()->json(['message' => 'ログアウトしました'], 200);
+    }
+
+    public function me() {
+        return response()->json(auth()->user(), 200);
+    }
+
+    public function refresh() {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token) {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
 }
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -50,5 +51,13 @@ class User extends Authenticatable
 
     public function works() {
         return $this->hasMany('App\Models\Work');
+    }
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
     }
 }
