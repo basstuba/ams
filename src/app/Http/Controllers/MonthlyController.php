2b4b836dba3aed6c7b7d6ce769be77986f21a684
nutka <tuba4080@outlook.com>
2024-11-09 15:30:15 +0100

    MonthlyController作成

--- src/app/Http/Controllers/MonthlyController.php
+++ src/app/Http/Controllers/MonthlyController.php
@@ -17,7 +17,49 @@ class MonthlyController extends Controller
             $monthAfter = Work::getAfterMonth($thisMonth);
 
             $userData = User::find($request->user_id);
-            $workLists = Work::getWorkLists($userData, $thisMonth);
+            $workLists = Work::getWorkLists($request->user_id, $thisMonth);
+
+            return response()->json([
+                'this_month' => $thisMonth,
+                'month_before' => $monthBefore,
+                'month_after' => $monthAfter,
+                'user_data' => $userData,
+                'work_lists' => $workLists
+            ], 200);
+        } catch(Exception $error) {
+            return response()->json(['error' => 'データの取得に失敗しました'], 500);
+        }
+    }
+
+    public function showBefore(Request $request) {
+        try {
+            $thisMonth = $request->month_before;
+            $monthBefore = Work::getBeforeMonth($thisMonth);
+            $monthAfter = Work::getAfterMonth($thisMonth);
+
+            $userData = User::find($request->user_id);
+            $workLists = Work::getWorkLists($request->user_id, $thisMonth);
+
+            return response()->json([
+                'this_month' => $thisMonth,
+                'month_before' => $monthBefore,
+                'month_after' => $monthAfter,
+                'user_data' => $userData,
+                'work_lists' => $workLists
+            ], 200);
+        } catch(Exception $error) {
+            return response()->json(['error' => 'データの取得に失敗しました'], 500);
+        }
+    }
+
+    public function showAfter(Request $request) {
+        try {
+            $thisMonth = $request->month_after;
+            $monthBefore = Work::getBeforeMonth($thisMonth);
+            $monthAfter = Work::getAfterMonth($thisMonth);
+
+            $userData = User::find($request->user_id);
+            $workLists = Work::getWorkLists($request->user_id, $thisMonth);
 
             return response()->json([
                 'this_month' => $thisMonth,
