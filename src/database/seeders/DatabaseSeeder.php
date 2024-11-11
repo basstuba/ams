29da5bdb73a9712d518d9a9480fb97e858c1d3b8
nutka <tuba4080@outlook.com>
2024-11-11 14:56:16 +0100

    ダミーユーザーデータ作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        User::factory(50)->create();
     }
 }
